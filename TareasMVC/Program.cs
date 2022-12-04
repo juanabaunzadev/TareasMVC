cac98285966a9aed7b65987d7ed455d0cb99fc16
Juan Abaunza <jabaunzagarcia@gmail.com>
2022-12-04 09:24:20 -0600

    First Entities Added

--- TareasMVC/Program.cs
+++ TareasMVC/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TareasMVC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer("name=DefaultConnection"));
 
 var app = builder.Build();
 
